f994a23400934e451e5a98aaf8aa73fa966af5ef
Kader Kaya <kaader.kaayaa@gmail.com>
2025-11-14 19:54:52 +0300

    fixed error handler

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,8 +1,18 @@
-import { Response } from "express";
+import { ErrorRequestHandler, NextFunction, Response, Request } from "express";
 import ResponseHelper from "./responseHelper.js";
 import ApiError from "./apiError.js";
 
-export default function errorHandler(res: Response, error: any) {
+const errorHandler: ErrorRequestHandler = (
+    // simdi burda TS fonksiyonu error handler olarak algilamadigi icin yani middleware olarak 
+    //algilamasi icin ErrorRequestHandler bunu ekliyoruz.
+    //PathParams olarak algilar
+    // error handlerde parametreleri err,req,res,next seklinde gondermezsek yine hata akliyoruz
+    //bu kisma yeni hatalar eklenebilir validation error gibi..
+    error: any,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     if (error instanceof ApiError) {
         return ResponseHelper.sendError(res, error.message, error.statusCode)
     }
@@ -10,4 +20,5 @@ export default function errorHandler(res: Response, error: any) {
         return ResponseHelper.sendError(res, 'Token Error', 401)
     }
     return ResponseHelper.sendError(res, 'Server Error', 500)
-}
+};
+export default errorHandler;
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ dotenv.config();
 import express from "express";
 const PORT = process.env.PORT;
 const app = express();
-import DB from './config/conf.js';
+import connectDB from './config/conf.js';
 import UserRouter from "./routes/user.js";
-// import ErrorHandler from "./helpers/errorHandler.js";
+import ErrorHandler from "./helpers/errorHandler.js";
 
 app.use(express.json());
-DB();
+connectDB();
+app.use(ErrorHandler)
 app.use("/user",UserRouter)
 app.listen(PORT!, ()=>{
     console.log(`*****************->PORT:${PORT}`);
